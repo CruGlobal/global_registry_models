f1e74db40c78cf2e84c8dbdf92690f45aef4556e
Sheldon Dueck <sheldon.dueck@gmail.com>
2015-09-23 15:43:12 -0400

    Use rack-cas single sign out

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'rack-cas/session_store/active_record'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -24,6 +25,7 @@ module GlobalRegistryInterface
     config.active_record.raise_in_transactional_callbacks = true
 
     config.rack_cas.server_url = 'https://thekey.me/cas/'
+    config.rack_cas.session_store = RackCAS::ActiveRecordStore
 
     config.active_job.queue_adapter = :sidekiq
 
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
-# Be sure to restart your server when you modify this file.
+require 'rack-cas/session_store/rails/active_record'
 
-Rails.application.config.session_store :cookie_store, key: '_global-registry-interface_session'
+Rails.application.config.session_store ActionDispatch::Session::RackCasActiveRecordStore
