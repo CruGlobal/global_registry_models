b4cc91a957f011ea158ac469f2963838d19f5cf9
Sheldon Dueck <sheldon.dueck@gmail.com>
2015-09-25 17:58:17 -0400

    Add aws ses actionmailer config

--- config/initializers/global_registry.rb
+++ config/initializers/global_registry.rb
@@ -1,4 +1,4 @@
-GlobalRegistry.access_token = Rails.application.secrets[:global_registry_access_token]
+GlobalRegistry.access_token = ENV['GLOBAL_REGISTRY_ACCESS_TOKEN']
 raise 'No Global Registry access token specified!' unless GlobalRegistry.access_token.present?
 
 GlobalRegistry.base_url = Rails.env.production? ? 'https://api.global-registry.org/' : 'https://stage-api.global-registry.org/'
