75e7abe7a8749eeb569a1bd6b71a703ff667393c
Sheldon Dueck <sheldon.dueck@gmail.com>
2015-10-05 15:18:16 -0400

    Make more room for filters

--- app/helpers/entities_helper.rb
+++ app/helpers/entities_helper.rb
@@ -2,8 +2,8 @@ module EntitiesHelper
 
   def filters_text_field_tag(name:, label:)
     safe_join [
-      label_tag(label),
-      text_field_tag("filters[#{ name }]", params[:filters].try(:[], name), placeholder: label, class: 'form-control')
+      label_tag("filters[#{ name }]", label, class: 'control-label col-sm-4 text-right'),
+      content_tag(:div, text_field_tag("filters[#{ name }]", params[:filters].try(:[], name), placeholder: label, class: 'form-control'), class: 'col-sm-8')
     ]
   end
 
