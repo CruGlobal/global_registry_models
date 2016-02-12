ecd6755f852e476f819c44abf6a75776fd468da8
Daniel Fugere <danielfugere@administrators-MacBook-Pro.local>
2016-02-12 13:07:31 -0800

    added handling for csv job timeout.

--- app/jobs/export_csv_job.rb
+++ app/jobs/export_csv_job.rb
@@ -2,10 +2,11 @@ class ExportCsvJob < ActiveJob::Base
   queue_as :default
 
   def perform(entity_class_name:, filters:, email:)
-    GlobalRegistry.access_token = Rails.application.secrets[:global_registry_access_token]
     ExportCsv.new(entity_class: "GlobalRegistryModels::Entity::#{entity_class_name.singularize.classify}"
              .safe_constantize, filters: filters, email: email).export! do |file_name, file|
       ExportCsvMailer.send_export(email, file_name, file.path).deliver_now
     end
+  rescue Net::HTTPGatewayTimeOut
+    ExportCsvMailer.send_export(email, entity_class_name).deliver_now
   end
 end
--- app/mailers/export_csv_mailer.rb
+++ app/mailers/export_csv_mailer.rb
@@ -4,4 +4,10 @@ class ExportCsvMailer < ApplicationMailer
     attachments[file_name] = File.read(file_path)
     mail to: email, subject: 'Your requested CSV export from Nebo'
   end
+
+  def send_export(email, file_name, file_path)
+    @file_name = file_name
+    attachments[file_name] = File.read(file_path)
+    mail to: email, subject: 'Your requested CSV export from Nebo'
+  end
 end
