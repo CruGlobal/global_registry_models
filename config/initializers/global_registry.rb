c90055f860a14b8d711f5e973052e19896f00c77
Daniel Fugere <danielfugere@administrators-MacBook-Pro.local>
2016-02-16 14:00:37 -0800

    readded the .env.sample file and changed the api_url var name

--- config/initializers/global_registry.rb
+++ config/initializers/global_registry.rb
@@ -1,2 +1,2 @@
 GlobalRegistry.access_token = Rails.application.secrets[:global_registry_access_token]
-GlobalRegistry.base_url = ENV['API_URL']
+GlobalRegistry.base_url = ENV['GLOBAL_REGISTRY_API_URL']
